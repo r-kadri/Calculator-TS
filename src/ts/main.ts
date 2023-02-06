f8d88a42833853d617c9ea9e628b61b9bea393d0
Ryan Kadri <ryankadri.dev@gmail.com>
2023-02-06 15:28:26 +0100

    test addition

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,7 @@
-console.log("hello world");
+import { Add } from "./Add";
+import { Constant } from "./Constant";
+
+// tests
+let c = new Constant(10);
+let addition = new Add(c, new Add(new Constant(5), c));
+console.log(addition.toString());
