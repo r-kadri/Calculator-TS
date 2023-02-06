12eeb336c092c3d75db471a1a8102bf247d9d8e9
Ryan Kadri <ryankadri.dev@gmail.com>
2023-02-06 15:27:22 +0100

    modified:   src/ts/Operation.ts

--- src/ts/Operation.ts
+++ src/ts/Operation.ts
@@ -4,14 +4,18 @@ import { Expression } from "./Expression";
  * Represent the operation between 2 operands
  */
 export abstract class Operation extends Expression {
-    private op1: number;
-    private op2: number;
+    protected op1: Expression;
+    protected op2: Expression;
     private sign: string;
 
-    constructor(op1: number, op2: number, sign: string) {
+    constructor(op1: Expression, op2: Expression, sign: string) {
         super();
         this.op1 = op1;
         this.op2 = op2;
         this.sign = sign;
+    }
+
+    public toString(): string {
+        return '(' + this.op1.toString() + ') ' + this.sign + ' (' + this.op2.toString()  + ')';
     }
 }
