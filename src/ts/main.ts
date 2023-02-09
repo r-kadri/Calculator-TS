fdb2f6e59d2f202127f0c891fd6e0c3d9f72a7cf
Ryan Kadri <ryankadri.dev@gmail.com>
2023-02-09 18:30:13 +0100

    addition 1 sign works

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,13 +1,65 @@
+const signs: string[] = ['+', '-'];
+const prioritySigns: string[] = ['*', '/'];
 const cScreen = document.getElementsByClassName("screen")[0];
 
 let clearButton = document.getElementsByClassName("clear")[0];
 clearButton.addEventListener('click', () => clear())
 
+let lastIsSign = true;
+
 let keys = document.querySelectorAll(".keys span");
 keys.forEach((key) => {
-    key.addEventListener('click', () => cScreen.innerHTML = key.innerHTML);
+    key.addEventListener('click', () => {
+        if(key.classList.contains("operator") || key.innerHTML == '.') {
+            if(lastIsSign) return;
+            lastIsSign = true;
+            cScreen.innerHTML += key.innerHTML;
+            return;
+
+        } else if(key.classList.contains("eval")) {
+            if(!lastIsSign)
+                res();
+
+            return;
+        };
+        
+        lastIsSign = false;
+        cScreen.innerHTML += key.innerHTML
+    });
 })
 
 function clear() {
     cScreen.innerHTML = "";
+    lastIsSign = true;
+}
+
+function res() {
+    let stop: boolean = false;
+    prioritySigns.forEach((sign) => {
+        if(cScreen.innerHTML.includes(sign)) {
+            stop = true;
+            return;
+        }
+    });
+    if(stop) return;
+
+    let part = "";
+    let s: string | null = null;
+    let finalResult: number | null = null;
+    for (let i = 0; i < cScreen.innerHTML.length; i++) {
+        if(signs.some( sign => sign == cScreen.innerHTML[i])) {
+            if(finalResult != null) {
+                // TODO (operation plusieurs signes)
+            } else {
+                finalResult = +part;
+                part = "";
+            }
+        } else {
+            part += cScreen.innerHTML[i];
+        }
+    }
+    if(finalResult) {
+        finalResult += +part;
+        cScreen.innerHTML = finalResult?.toString();
+    }
 }
